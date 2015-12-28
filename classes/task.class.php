e610d52f468ecb106a75947592c77ffeebc4d085
Andras Serfozo <andras@anyvan.com>
2015-12-28 17:27:42 +0100

    Run task

--- classes/task.class.php
+++ classes/task.class.php
@@ -1,15 +1,33 @@
 <?php
 class Task
 {
-	public $exec;       // string | execute line
-	public $secs;       // int    | frequency in seconds
-	public $log;        // string | log identifier
-	public $pid = null; // PID
+	public $exec;        // string | execute line
+	public $secs;        // int    | frequency in seconds
+	public $log;         // string | log identifier
+	public $pid = null;  // PID
+	public $nextRun = 0; // Next run timestamp
 
 	public function __construct($data = []) {
-		$this->exec = isset($data['exec']) ? $data['exec'] : null;
-		$this->secs = isset($data['secs']) ? $data['secs'] : null;
+		$this->exec = $data['exec'];
+		$this->secs = isset($data['secs']) ? (int)$data['secs'] : 0;
 		$this->log = isset($data['log']) ? $data['log'] : null;
+	}
+
+	public function run() {
+		if ($this->log) {
+			$log = __DIR__."/../log/{$this->log}.txt";
+			file_put_contents($log, "\n-----\n".date('Y-m-d H:i:s')."\n-----\n\n", FILE_APPEND);
+			$log = " >> $log 2>> $log";
+		} else {
+			$log = ' > /dev/null 2> /dev/null';
+		}
+
+		$this->pid = exec($this->exec.$log.' & echo $!');
+		$this->nextRun = time() + $this->secs;
+	}
+
+	public function shouldRun() {
+		return true;
 	}
 
 	public function isRunning() {
