effce9686dcfbf8855f2c075daf84c8d16ded0af
Andras Serfozo <andras@anyvan.com>
2016-01-11 12:03:12 +0100

    Read self dir for config

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once(__DIR__.'/classes/task.class.php');
 $tasks = [];
 
 // Load tasks from json
-$tasks_arr = json_decode(file_get_contents('tasks.json'), true);
+$tasks_arr = json_decode(file_get_contents(__DIR__.'/tasks.json'), true);
 foreach ($tasks_arr as $task_arr) {
 	$task = new Task($task_arr);
 	$tasks[] = $task;
@@ -15,7 +15,7 @@ while (true) {
 	foreach ($tasks as $task) {
 		if (!$task->isRunning() && $task->shouldRun()) {
 			$task->run();
-		}		
+		}
 	}
 
 	echo 'Mem: '.number_format(memory_get_usage())."\n";
