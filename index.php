660ac0b4fb5b4f7f47940e2a3fee70713b4c4486
Andras Serfozo <subztep@gmail.com>
2016-02-16 14:02:07 +0000

    Tasks file as param

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 include(__DIR__.'/vendor/autoload.php');
 
+// Get parameter
+$opt = getopt('f:');
+$rulesFile = isset($opt['f']) ? $opt['f'] : __DIR__.'/tasks.json';
+
+// Init taskrunner
 try {
-	$runner = new PhpCron\TaskRunner(__DIR__.'/tasks.json');
+	$runner = new PhpCron\TaskRunner($rulesFile);
 } catch (Exception $e) {
 	echo 'Init error: '.$e->getMessage()."\n";
 	exit(1);
 }
 
+// Run task(s)
 $runner->run();
